eaa5995f34af26baed5713983279897ddf0594e1
daihuku99 <dai9huku@gmail.com>
2020-03-08 15:46:30 +0900

    modified

--- app/controllers/bookslists_controller.rb
+++ app/controllers/bookslists_controller.rb
@@ -1,44 +1,47 @@
 class BookslistsController < ApplicationController
   def new
-  	  @list = List.new
+  	  @book = Book.new
   end
 
   def create
-  	  list = List.new(list_params)
-      if list.save
+  	  @book = Book.new(book_params)
+      if @book.save
         flash[:notice] = 'Book was successfully created.'
-  	    redirect_to bookslist_path(list.id)
+  	    redirect_to book_path(@book.id)
+      else
+        @books = Book.all
+        render :index
       end
   end
 
   def index
-  	  @lists = List.all
-      @list = List.new
+  	  @books = Book.all
+      @book = Book.new
   end
 
   def show
-  	  @list = List.find(params[:id])
+  	  @book = Book.find(params[:id])
   end
 
   def edit
-  	  @list = List.find(params[:id])
+  	  @book = Book.find(params[:id])
   end
 
   def update
-  	  list = List.find(params[:id])
-  	  list.update(list_params)
-  	  redirect_to bookslist_path(list.id), notice: 'Book was successfully updated.'
+  	  book = Book.find(params[:id])
+  	  book.update(book_params)
+  	  redirect_to book_path(book.id), notice: 'Book was successfully updated.'
   end
 
   def destroy
-  	  list = List.find(params[:id])
-  	  list.destroy
-  	  redirect_to bookslists_path
+  	  book = Book.find(params[:id])
+  	  book.destroy
+  	  redirect_to books_path
   end
 
   private
 
-  def list_params
-  	params.require(:list).permit(:title, :body)
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_040618) do
+ActiveRecord::Schema.define(version: 2020_03_07_101207) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
